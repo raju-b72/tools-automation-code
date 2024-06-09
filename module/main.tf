ea937cfe2911180b6c4cc1c4bc088c7fa4828516
rajendra <rajendrammarboyana@gmail.com>
2024-06-09 15:08:38 +0100

    prometheus install through automation

--- module/main.tf
+++ module/main.tf
@@ -14,13 +14,7 @@ resource "aws_route53_record" "record" {
   name    = var.tool_name
   type    = "A"
   zone_id = var.zone_id
-  records = [aws_instance.instance.public_ip]
-  ttl = 30
-}
-resource "aws_route53_record" "record-internal" {
-  name    = "${var.tool_name}-internal"
-  type    = "A"
-  zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
   ttl = 30
 }
+
