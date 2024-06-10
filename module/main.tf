72d473c7ccf5c981c8acc4c86fe258cc90b31206
rajendra <rajendrammarboyana@gmail.com>
2024-06-10 09:53:10 +0100

    spot instances

--- module/main.tf
+++ module/main.tf
@@ -62,7 +62,15 @@ resource "aws_route53_record" "record" {
   name    = var.tool_name
   type    = "A"
   zone_id = var.zone_id
+  records = [aws_instance.instance.public_ip]
+  ttl = 30
+}
+resource "aws_route53_record" "record-internal" {
+  name    = "${var.tool_name}-internal"
+  type    = "A"
+  zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
   ttl = 30
 }
+
 
