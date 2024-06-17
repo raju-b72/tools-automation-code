9d5f2c6e15e86a873a366c9f2748f83251d471d6
rajendra <rajendrammarboyana@gmail.com>
2024-06-17 21:53:29 +0100

    adding sonarqube

--- variables.tf
+++ variables.tf
@@ -20,6 +20,10 @@ variable "tools" {
       instance_type        = "t3.small"
       policy_resource_list = []
     }
+    sonarqube = {
+      instance_type        = "t3.large"
+      policy_resource_list = []
+    }
   }
 }
 
